48b8d58ee492647c31f4e3cbec8f18533a800f00
chesslindan-ops <chesslindan@gmail.com>
2025-11-09 14:49:44 +0530

    Update Lal.py

--- Lal.py
+++ Lal.py
@@ -72,7 +72,7 @@ async def links_command(interaction: discord.Interaction):
         embed.set_image(url="https://media.tenor.com/vkIBLYGz5O0AAAAM/im-going-to-send-nsfw-in-ur-dms.gif")
         embed.set_thumbnail(url="https://media.tenor.com/8YQU67-dvTUAAAAM/penis.gif")  # single followup only
         for _ in range(10):
-         await asyncio.sleep(0.5)
+            await asyncio.sleep(0.5)
             await interaction.followup.send(embed=embed)
         return
 
